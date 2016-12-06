78701a5774d78c16e65a12fe70f9e62b7298a9f1
jemd93 <hamster344>
2016-12-06 19:33:50 -0300

    Listo :D

--- ProyectoFinal/JuegoUnity/Assets/Scripts/TiltController.cs
+++ ProyectoFinal/JuegoUnity/Assets/Scripts/TiltController.cs
@@ -2,61 +2,68 @@
 using System.Collections;
 
 public class TiltController : MonoBehaviour {
-	float tiltSpeedSide = 1.0f;
-	float tiltSpeedUp = 0.5f;
-	float rotationSide = 0;
-	float rotationUp = 0;
-	// Use this for initialization
+	public float tiltSpeedSide = 1.0f;
+	public float tiltSpeedUp = 0.5f;
+
+
+	Quaternion originalRotation;
+	bool restoreRotation = false;
+
 	void Start () {
-	
+		originalRotation = transform.rotation;
+	}
+
+	private void moveRight(float speed) {
+		transform.RotateAround (transform.position, transform.forward, 45 * speed * Time.deltaTime);
+		restoreRotation = true;
+	}
+
+	private void moveLeft(float speed) {
+		transform.RotateAround (transform.position, -transform.forward, 45 * speed * Time.deltaTime);
+		restoreRotation = true;
+	}
+
+	private void moveUp(float speed) {
+		transform.RotateAround (transform.position, transform.right, 45 * speed * Time.deltaTime);
+		restoreRotation = true;
+	} 
+
+	private void moveDown(float speed) {
+		transform.RotateAround (transform.position, -transform.right, 45 * speed * Time.deltaTime);
+		restoreRotation = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				if (rotationSide < 40) {
-					transform.RotateAround (transform.position, transform.forward, 45 * tiltSpeedSide * Time.deltaTime);
-					rotationSide += 1;
-				}
-			} 
-			else if (Input.GetKey (KeyCode.LeftArrow)) {
-				if (rotationSide > -40) {
-					transform.RotateAround (transform.position, -transform.forward, 45 * tiltSpeedSide * Time.deltaTime);
-					rotationSide -= 1;
-				}
-			} 
-			else {
-				if (rotationSide > 0) {
-					transform.RotateAround (transform.position, -transform.forward, 45 * tiltSpeedSide * Time.deltaTime);
-					rotationSide -= 1;
-				}
-				if (rotationSide < 0) {
-					transform.RotateAround (transform.position, transform.forward, 45 * tiltSpeedSide * Time.deltaTime);
-					rotationSide += 1;
-				}
+			if ( (Input.GetKey (KeyCode.RightArrow)) || (Input.GetKey (KeyCode.LeftArrow)) 
+				|| (Input.GetKey (KeyCode.W)) || (Input.GetKey (KeyCode.S))) {
+
+				float angleZ = transform.localEulerAngles.z;
+				float angleX = transform.localEulerAngles.x;
+				angleX = (angleX > 180) ? angleX - 360 : angleX;
+				angleZ = (angleZ > 180) ? angleZ - 360 : angleZ;
+
+				if (Input.GetKey (KeyCode.RightArrow)) {
+					if (angleZ < 30) 
+						moveRight (tiltSpeedSide);
+				} 
+				else if (Input.GetKey (KeyCode.LeftArrow)) {
+					if (angleZ > -30)
+						moveLeft (tiltSpeedSide);
+				} 
+					
+				if (Input.GetKey (KeyCode.W)) {
+					if (angleX < 16)
+						moveUp (tiltSpeedUp);
+				} 
+				else if (Input.GetKey (KeyCode.S)) {
+					if (angleX > -16) 
+						moveDown (tiltSpeedUp);
+				} 
 			}
-			if (Input.GetKey (KeyCode.W)) {
-				if (rotationUp < 40) {
-					transform.RotateAround (transform.position, transform.right, 45 * tiltSpeedUp * Time.deltaTime);
-					rotationUp += 1;
-				}
-			} 
-			else if (Input.GetKey (KeyCode.S)) {
-				if (rotationUp > -40) {
-					transform.RotateAround (transform.position, -transform.right, 45 * tiltSpeedUp * Time.deltaTime);
-					rotationUp -= 1;
-				}
-			} 
 			else {
-				if (rotationUp > 0) {
-					transform.RotateAround (transform.position, -transform.right, 45 * tiltSpeedUp * Time.deltaTime);
-					rotationUp -= 1;
-				}
-				if (rotationUp < 0) {
-					transform.RotateAround (transform.position, transform.right, 45 * tiltSpeedUp * Time.deltaTime);
-					rotationUp += 1;
-				}
+				transform.localRotation = Quaternion.Lerp(transform.localRotation,originalRotation,Time.time * 0.005f);
 			}
 		}
 	}
